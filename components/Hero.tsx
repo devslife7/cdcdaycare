b6809213bd5678f3709c882ec59070c52870162d
Marcos Velasco <devslife7@gmail.com>
2023-08-25 17:12:36 -0400

    added all line decorations

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,9 +18,12 @@ export default function Hero() {
             <div className="h-full bg-black bg-opacity-50">
                 <div className="flex items-center h-full text-white my-container">
                     <div className="max-w-4xl py-14 lg:py-20">
-                        <h1 className="text-4xl font-extrabold md:text-5xl lg:text-7xl">
+                        <h1 className="text-4xl font-extrabold md:text-5xl lg:text-7xl relative">
                             A Safe Haven for
                             <br /> Growing Minds
+                            <span className="font-medium text-primary absolute left-1 -top-[4.5rem]">
+                                ___
+                            </span>
                         </h1>
                         <p className="max-w-lg mt-5 mb-10 font-light text-md lg:leading-relaxed">
                             Our child-friendly spaces are designed to encourage exploration while providing a
