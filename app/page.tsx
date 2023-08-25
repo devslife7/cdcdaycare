e9a66e42f4dbe886fc4e76f6965c7433f7160742
Marcos Velasco <devslife7@gmail.com>
2023-08-25 15:20:26 -0400

    added colors to theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import AboutUs from "@/components/AboutUs"
+import ContactUs from "@/components/ContactUs"
+import Hero from "@/components/Hero"
+import KeyPoints from "@/components/KeyPoints"
+import Philosophy from "@/components/Philosophy"
+import Footer from "@/components/layout/Footer"
+import Navbar from "@/components/layout/Navbar"
+
 export default function Home() {
-  return <div>Hello world</div>
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <KeyPoints />
+      <Philosophy />
+      <AboutUs />
+      <ContactUs />
+      <Footer />
+    </div>
+  )
 }
