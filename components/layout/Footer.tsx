9c78830ebdc32f3d243aa8349b0f91ae63f59997
Marcos Velasco <devslife7@gmail.com>
2023-08-25 15:12:12 -0400

    added layout components

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="container-custom flex h-1/4 items-center justify-center">
+      <div className="text-5xl">Footer</div>
+    </div>
+  )
+}
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="container-custom flex h-1/4 items-center justify-center">
+      <div className="text-5xl">Navbar</div>
+    </div>
+  )
+}
