a23b426782b045763ba2548e5eefb2798d7d5dcf
Marcos Velasco <devslife7@gmail.com>
2023-08-26 18:20:22 -0400

    updated hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image"
-import heroBackground from "@/public/img/daycareHero.jpeg"
+// import heroBackground from "@/public/img/daycareHero.jpeg"
+import heroBackground from "../public/img/daycareHero.jpeg"
 import { Link as ScrollLink } from "react-scroll"
 import Button from "./shared/Button"
 
