08d05978f8fdd1b3c87327da3491308bdfbc124d
Marcos Velasco <devslife7@gmail.com>
2023-08-28 18:48:17 -0400

    translated about us and philosoby sections

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import aboutUsSection from "@/public/images/about-section.jpg"
 import RevealMotion from "./shared/RevealMotion"
+import Translator from "./shared/Translator"
 
 export default function AboutUs() {
     return (
@@ -8,15 +9,12 @@ export default function AboutUs() {
             <RevealMotion className="my-container lg:flex lg:flex-row-reverse lg:justify-evenly space-y-10">
                 <div className="max-w-lg space-y-6 lg:px-4 mx-auto">
                     <h2 className="text-purple text-5xl mt-8 relative">
-                        ABOUT US<span className="absolute left-0 top-2">___</span>
+                        <Translator content="AboutUsTitle" />
+                        <span className="absolute left-0 top-2">___</span>
                     </h2>
 
                     <p className="text-xl font-[250] leading-10">
-                        At CDC Daycare, we believe that every child deserves a nurturing and inspiring
-                        environment where they can explore, learn, and flourish. Established with love and
-                        passion for early childhood development, our daycare center is dedicated to providing
-                        a safe, stimulating, and loving space for children to embark on an incredible journey
-                        of growth and discovery.
+                        <Translator content="AboutUsSubtext" />
                     </p>
                 </div>
 
--- components/Philosophy.tsx
+++ components/Philosophy.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import philoSection from "@/public/images/philosophy-section.jpg"
 import RevealMotion from "./shared/RevealMotion"
+import Translator from "./shared/Translator"
 
 export default function Philosophy() {
     return (
@@ -8,13 +9,11 @@ export default function Philosophy() {
             <RevealMotion className="my-container lg:flex lg:justify-evenly space-y-10">
                 <div className="max-w-xl space-y-6 lg:px-4 mx-auto">
                     <h2 className="text-primary text-5xl mt-8 relative">
-                        PHILOSOPHY<span className=" absolute left-0 top-2">___</span>
+                        <Translator content="philosophyTitle" />
+                        <span className=" absolute left-0 top-2">___</span>
                     </h2>
                     <p className="text-xl font-extralight leading-10">
-                        We believe that with love and care every child can learn. That is why we provide all
-                        the elements for a strong foundation of success. We take pride in creating a healthy
-                        environment with wholesome activities. We purposely fill our time with active,
-                        fun-filled programs that engage the whole child's developing mind and body.
+                        <Translator content="philosophySubtext" />
                     </p>
                 </div>
                 <Image className="mx-auto" src={philoSection} unoptimized alt="Child doing homework" />
