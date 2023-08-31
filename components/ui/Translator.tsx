63f063d75e000ddda47e2a9f85de9fc594b69699
Marcos Velasco <devslife7@gmail.com>
2023-08-31 12:13:52 -0400

    removed some doubplicate files

--- components/ui/Translator.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import translator from "@/data/translator.json"
-import { useGlobalStore } from "@/state/store"
-
-export default function Translator({ content }: { content: keyof typeof translator }) {
-    const { lang } = useGlobalStore()
-    return translator[content][lang].toString()
-}
