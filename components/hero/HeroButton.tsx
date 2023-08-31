484a9c8a2689de5b777fa5935fb39d851de587af
Marcos Velasco <devslife7@gmail.com>
2023-08-31 17:45:20 -0400

    testing

--- components/hero/HeroButton.tsx
+++ components/hero/HeroButton.tsx
@@ -7,6 +7,9 @@ export default function HeroButton() {
     return (
         <ScrollLink to="contact" className={buttonStyles({ size: "lg" })}>
             <Translator content="heroButton" />
+            <Translator content="heroButton" />
+            <Translator content="heroButton" />
+            <Translator content="heroButton" />
         </ScrollLink>
     )
 }
