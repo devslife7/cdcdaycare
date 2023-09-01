270a299500a37a243fcf5f8f38564bc8ed3ead62
Marcos Velasco <devslife7@gmail.com>
2023-09-01 08:55:03 -0400

    about us better content

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -7,21 +7,24 @@ export default function AboutUs() {
     return (
         <section id="about-us" className="py-24">
             <FadeIn className="my-container lg:flex lg:flex-row-reverse lg:justify-evenly space-y-10">
-                <div className="max-w-lg space-y-6 lg:px-4 mx-auto">
+                <div className="max-w-xl space-y-6 lg:px-4 mx-auto">
                     <h2 className="text-purple text-5xl mt-8 relative">
                         <Translator content="AboutUsTitle" />
                         <span className="absolute left-0 top-2">___</span>
                     </h2>
 
-                    <p className="text-xl font-[250] leading-10">
-                        <Translator content="AboutUsSubtext" />
+                    <p className="text-lg font-[250] leading-8 text-purple-950">
+                        <Translator content="AboutUsSubtext1" />
+                    </p>
+                    <p className="text-lg font-[250] leading-8 text-purple-950">
+                        <Translator content="AboutUsSubtext2" />
                     </p>
                 </div>
 
                 <Image
                     src={aboutUsSection}
                     unoptimized
-                    alt="Child doing homework"
+                    alt="Child playing"
                     className="mx-auto shadow-custom-left shadow-purple"
                 />
             </FadeIn>
--- components/Philosophy.tsx
+++ components/Philosophy.tsx
@@ -7,12 +7,12 @@ export default function Philosophy() {
     return (
         <section id="philosophy" className="bg-secondary py-24 text-white">
             <FadeIn className="my-container lg:flex lg:justify-evenly space-y-10">
-                <div className="max-w-xl space-y-6 lg:px-4 mx-auto">
+                <div className="max-w-xl space-y-6 lg:px-4 lg:mt-10 mx-auto">
                     <h2 className="text-primary text-5xl mt-8 relative">
                         <Translator content="philosophyTitle" />
                         <span className=" absolute left-0 top-2">___</span>
                     </h2>
-                    <p className="text-xl font-extralight leading-10">
+                    <p className="text-lg font-extralight leading-10 text-primary-100">
                         <Translator content="philosophySubtext" />
                     </p>
                 </div>
