c52307d5e9a68f86f9181c8cf39bd3c238c54145
Marcos Velasco <devslife7@gmail.com>
2023-12-29 22:18:58 -0500

    removed old dictionary implementation

--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -10,12 +10,6 @@ import Navbar from "@/components/layout/navbar/Navbar"
 export default async function Main({ params: { lang } }: { params: { lang: string } }) {
   const dict = await getDictionary(lang)
   return (
-    // <div className="flex justify-center h-screen items-center text-5xl">
-    //   <div>
-    //     <h2>{dict.hello.title}</h2>
-    //     <p>{dict.hello.text}</p>
-    //   </div>
-    // </div>
     <>
       <Navbar dictionary={dict.hello} />
       <Hero dictionary={dict.hello} />
