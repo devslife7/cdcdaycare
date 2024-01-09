0b19bea100a1784e16a2e4ddbc160777fa162e78
Marcos Velasco <devslife7@gmail.com>
2024-01-08 23:43:04 -0500

    testing fade in comp

--- components/KeyPoints.tsx
+++ components/KeyPoints.tsx
@@ -3,26 +3,26 @@ import { BsFillRocketFill } from "react-icons/bs"
 import { AiOutlineSafety } from "react-icons/ai"
 import FadeIn from "./ui/fadeIn"
 
-const KeyPoints = ({ dictionary }: { dictionary: any }) => (
-  <section className="my-container">
-    <FadeIn className="flex flex-wrap justify-start gap-12 my-20 md:gap-0 lg:justify-evenly">
-      <div className="max-w-xs p-2 text-center mx-auto">
-        <BsFillRocketFill className="mb-6 mx-auto text-7xl text-purple" />
-        <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.mission.title}</h1>
-        <p className="opacity-80 font-light">{dictionary.mission.subtitle}</p>
-      </div>
-      <div className="max-w-xs p-2 text-center mx-auto">
-        <AiOutlineSafety className="mb-6 mx-auto text-7xl text-green-400" />
-        <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.safety.title}</h1>
-        <p className="opacity-80 font-light">{dictionary.safety.subtitle}</p>
-      </div>
-      <div className="max-w-xs p-2 text-center mx-auto">
-        <MdFamilyRestroom className="mb-6 mx-auto text-7xl text-orange" />
-        <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.parents.title}</h1>
-        <p className="opacity-80 font-light">{dictionary.parents.subtitle}</p>
-      </div>
-    </FadeIn>
-  </section>
-)
-
-export default KeyPoints
+export default function KeyPoints({ dictionary }: { dictionary: any }) {
+  return (
+    <section className="my-container">
+      <FadeIn className="flex flex-wrap justify-start gap-12 my-20 md:gap-0 lg:justify-evenly">
+        <div className="max-w-xs p-2 text-center mx-auto">
+          <BsFillRocketFill className="mb-6 mx-auto text-7xl text-purple" />
+          <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.mission.title}</h1>
+          <p className="opacity-80 font-light">{dictionary.mission.subtitle}</p>
+        </div>
+        <div className="max-w-xs p-2 text-center mx-auto">
+          <AiOutlineSafety className="mb-6 mx-auto text-7xl text-green-400" />
+          <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.safety.title}</h1>
+          <p className="opacity-80 font-light">{dictionary.safety.subtitle}</p>
+        </div>
+        <div className="max-w-xs p-2 text-center mx-auto">
+          <MdFamilyRestroom className="mb-6 mx-auto text-7xl text-orange" />
+          <h1 className="mb-1 lg:mb-3 text-2xl">{dictionary.parents.title}</h1>
+          <p className="opacity-80 font-light">{dictionary.parents.subtitle}</p>
+        </div>
+      </FadeIn>
+    </section>
+  )
+}
--- components/ui/fadeIn.tsx
+++ components/ui/fadeIn.tsx
@@ -6,38 +6,38 @@ const FadeInStaggerContext = createContext(false)
 const viewport = { once: true, margin: "0px" }
 
 export default function FadeIn({ ...props }) {
-    const shouldReduceMotion = useReducedMotion()
-    const isInStaggerGroup = useContext(FadeInStaggerContext)
+  const shouldReduceMotion = useReducedMotion()
+  const isInStaggerGroup = useContext(FadeInStaggerContext)
 
-    return (
-        <motion.div
-            variants={{
-                hidden: { opacity: 0, y: shouldReduceMotion ? 0 : 75 },
-                visible: { opacity: 1, y: 0 },
-            }}
-            transition={{ duration: 0.8 }}
-            {...(isInStaggerGroup
-                ? {}
-                : {
-                      initial: "hidden",
-                      whileInView: "visible",
-                      viewport,
-                  })}
-            {...props}
-        />
-    )
+  return (
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, y: shouldReduceMotion ? 0 : 75 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ duration: 0.8 }}
+      {...(isInStaggerGroup
+        ? {}
+        : {
+            initial: "hidden",
+            whileInView: "visible",
+            viewport,
+          })}
+      {...props}
+    />
+  )
 }
 
 export const FadeInStagger = ({ faster = false, ...props }) => {
-    return (
-        <FadeInStaggerContext.Provider value={true}>
-            <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={viewport}
-                transition={{ staggerChildren: faster ? 0.12 : 0.2 }}
-                {...props}
-            />
-        </FadeInStaggerContext.Provider>
-    )
+  return (
+    <FadeInStaggerContext.Provider value={true}>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={viewport}
+        transition={{ staggerChildren: faster ? 0.12 : 0.2 }}
+        {...props}
+      />
+    </FadeInStaggerContext.Provider>
+  )
 }
