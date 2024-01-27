f16094288510bcf80542c25f269f40a31b93bb85
Marcos Velasco <devslife7@gmail.com>
2024-01-27 04:15:37 -0500

    put back components

--- components/Philosophy.tsx
+++ components/Philosophy.tsx
@@ -4,7 +4,7 @@ import FadeIn from "./ui/fadeIn"
 
 export default function Philosophy({ dictionary }: { dictionary: any }) {
   return (
-    <section id="philosophy" className="bg-secondary py-10 lg:py-24 text-white">
+    <section id="philosophy" className="bg-secondary py-10 lg:py-28 text-white">
       <FadeIn className="my-container grid lg:grid-cols-2 gap-16 justify-center content-center">
         <div className="max-w-2xl space-y-6 lg:px-4 mx-auto">
           <h2 className="text-primary text-3xl lg:text-5xl relative">
