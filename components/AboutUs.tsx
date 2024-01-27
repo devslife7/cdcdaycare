71844b4ce6d414dda44cd189cb5e11e5a5adf517
Marcos Velasco <devslife7@gmail.com>
2024-01-27 17:06:50 -0500

    align items center

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -6,19 +6,20 @@ export default function AboutUs({ dictionary }: { dictionary: any }) {
   return (
     <section id="about-us" className="py-5 lg:py-24">
       <FadeIn className="my-container grid justify-center mt-10 lg:grid-cols-2 gap-8 lg:gap-16">
-        <div className="max-w-xl mx-auto">
-          <h2 className="text-purple text-3xl lg:text-5xl relative mb-4 lg:mb-6">
-            {dictionary.title}
-            <span className="absolute left-0 top-2">___</span>
-          </h2>
-
-          <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950 mb-2 lg:mb-4">
-            {dictionary.subtitle1}
-          </p>
-          <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950">{dictionary.subtitle2}</p>
+        <div className="max-w-xl grid items-center">
+          <div>
+            <h2 className="text-purple text-3xl lg:text-5xl relative mb-4 lg:mb-6">
+              {dictionary.title}
+              <span className="absolute left-0 top-2">___</span>
+            </h2>
+            <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950 mb-2 lg:mb-4">
+              {dictionary.subtitle1}
+            </p>
+            <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950">{dictionary.subtitle2}</p>
+          </div>
         </div>
 
-        <div className="relative h-96 max-w-xl self-start lg:order-first">
+        <div className="relative h-[400px] max-w-xl self-start lg:order-first">
           <Image
             src={aboutUsSection}
             alt="About image"
--- components/Philosophy.tsx
+++ components/Philosophy.tsx
@@ -6,16 +6,18 @@ export default function Philosophy({ dictionary }: { dictionary: any }) {
   return (
     <section id="philosophy" className="bg-secondary py-10 lg:py-28 text-white">
       <FadeIn className="my-container grid lg:grid-cols-2 gap-8 lg:gap-16 justify-center content-center">
-        <div className="max-w-xl mx-auto">
-          <h2 className="text-primary text-3xl lg:text-5xl relative mb-4 lg:mb-6">
-            {dictionary.title}
-            <span className=" absolute left-0 top-2">___</span>
-          </h2>
-          <p className="lg:text-lg lg:leading-loose font-extralight leading-relaxed text-primary-100">
-            {dictionary.subtitle}
-          </p>
+        <div className="max-w-lg grid items-center">
+          <div>
+            <h2 className="text-primary text-3xl lg:text-5xl relative mb-4 lg:mb-6">
+              {dictionary.title}
+              <span className=" absolute left-0 top-2">___</span>
+            </h2>
+            <p className="lg:text-lg lg:leading-loose font-extralight leading-relaxed text-primary-100">
+              {dictionary.subtitle}
+            </p>
+          </div>
         </div>
-        <div className="max-w-xl relative h-96">
+        <div className="max-w-xl relative h-[400px]">
           <Image
             className="mx-auto shadow-custom-right object-cover shadow-primary"
             fill
