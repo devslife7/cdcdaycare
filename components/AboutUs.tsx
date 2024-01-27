8b4eef07a6f32a426294a7322185fe5b36132f70
Marcos Velasco <devslife7@gmail.com>
2024-01-27 04:14:07 -0500

    removed backgrounds

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -5,15 +5,15 @@ import FadeIn from "./ui/fadeIn"
 export default function AboutUs({ dictionary }: { dictionary: any }) {
   return (
     <section id="about-us" className="py-5 lg:py-24">
-      <FadeIn className="my-container grid justify-center lg:grid-cols-2 gap-16">
-        <div className="space-y-6  s max-w-2xl mx-auto">
-          <h2 className="text-purple text-3xl lg:text-5xl mt-8 relative">
+      <FadeIn className="my-container grid justify-center mt-10 lg:grid-cols-2 gap-16">
+        <div className="space-y-6 max-w-2xl mx-auto">
+          <h2 className="text-purple text-3xl lg:text-5xl relative">
             {dictionary.title}
             <span className="absolute left-0 top-2">___</span>
           </h2>
 
-          <p className="lg:text-lg font-[250] leading-8 text-purple-950">{dictionary.subtitle1}</p>
-          <p className="lg:text-lg font-[250] leading-8 text-purple-950">{dictionary.subtitle2}</p>
+          <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950">{dictionary.subtitle1}</p>
+          <p className="lg:text-lg font-[250] leading-relaxed lg:leading-8 text-purple-950">{dictionary.subtitle2}</p>
         </div>
 
         <div className="relative h-96 max-w-2xl self-start lg:order-first">
--- components/Philosophy.tsx
+++ components/Philosophy.tsx
@@ -5,13 +5,15 @@ import FadeIn from "./ui/fadeIn"
 export default function Philosophy({ dictionary }: { dictionary: any }) {
   return (
     <section id="philosophy" className="bg-secondary py-10 lg:py-24 text-white">
-      <FadeIn className="my-container grid lg:grid-cols-2 gap-16 justify-center">
-        <div className="max-w-2xl space-y-6 lg:px-4 mx-auto bg-pink-400">
-          <h2 className="text-primary text-3xl lg:text-5xl mt-8 relative">
+      <FadeIn className="my-container grid lg:grid-cols-2 gap-16 justify-center content-center">
+        <div className="max-w-2xl space-y-6 lg:px-4 mx-auto">
+          <h2 className="text-primary text-3xl lg:text-5xl relative">
             {dictionary.title}
             <span className=" absolute left-0 top-2">___</span>
           </h2>
-          <p className="lg:text-lg font-extralight leading-loose bg-blue-400 text-primary-100">{dictionary.subtitle}</p>
+          <p className="lg:text-lg lg:leading-loose font-extralight leading-relaxed text-primary-100">
+            {dictionary.subtitle}
+          </p>
         </div>
         <div className="max-w-2xl relative h-96">
           <Image
